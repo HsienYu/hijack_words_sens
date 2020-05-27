63a83c3f11db9e55ee917a16e2c62cfa06a2b8c6
H.Y.C <chenghsienyu@aim.com>
2020-05-27 09:27:40 +0800

    update

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const Nonsensical = require("./nonsensical");
 const nonsensical = new Nonsensical();
+const axios = require('axios')
+const path = require('path');
+
 
 const express = require('express')
 const app = express()
+app.use(express.static(__dirname + '/static'));
+app.use('/images', express.static(__dirname + '/images'))
+
 
 global.window = {
 	fetch: (path) => {
@@ -26,12 +32,27 @@ const data_file_paths = {
 	verb: './data/verb.json',
 };
 
+app.get('/query', (req, res) => {
+	var s = nonsensical.generateSentence();
+	console.log(s);
+	res.send(JSON.stringify(s));
+	axios.post('http://localhost:8000/query', { "caption": `${s}` })
+		.catch(error => {
+			console.error(error)
+		})
+});
 
-app.get('/', function (req, res) {
-	nonsensical.load(data_file_paths, function () {
-		console.log(nonsensical.generateSentence());
-		res.send(nonsensical.generateSentence());
-	});
-})
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/static/index.html'));
+});
 
-app.listen(3000)
+app.get('/view', (req, res) => {
+	res.sendFile(path.join(__dirname, '/static/view.html'));
+});
+
+
+
+nonsensical.load(data_file_paths, function () {
+	console.log('server running...');
+	app.listen(3000);
+});
